a030751433acf50381fa27ff22ffb697fc10de21
Ayoub <ayoublaayar390@gmail.com>
2025-11-21 13:46:21 +0000

    update TP1: description des changements

--- Projects/React/User Managment/vite.config.js
+++ Projects/React/User Managment/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "https://github.com/Laayar/GitHub-Portfolio/tree/main/Projects/React/User%20Managment",
+});
